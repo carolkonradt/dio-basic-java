ddafd6756484f38d5c8518c0828891571bdcd843
Ana Carolina <carolkonradt41@gmail.com>
2024-05-01 16:42:16 -0300

    remembering Scanners

--- my-first-java-project/src/App.java
+++ my-first-java-project/src/App.java
@@ -1,5 +1,27 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Bem-vindos!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Meu nome é "+ nome + " "+sobrenome);
+        System.out.println("Tenho " +idade+ " anos.");
+        System.err.println("Minha altura é "+altura+ " m.");
     }
 }
